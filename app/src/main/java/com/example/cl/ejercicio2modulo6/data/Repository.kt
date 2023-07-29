61a40b35bdd21820320b25c0fc2310eec1266d95
Felipe Gonzalez <felipegonzalezurr@gmail.com>
2023-07-28 21:19:38 -0400

    Hito 1 completo

---
+++ app/src/main/java/com/example/cl/ejercicio2modulo6/data/Repository.kt
@@ -0,0 +1,19 @@
+package com.example.cl.ejercicio2modulo6.data
+
+import androidx.lifecycle.LiveData
+import com.example.cl.ejercicio2modulo6.data.local.Item
+import com.example.cl.ejercicio2modulo6.data.local.ItemDao
+
+class Repository(private val itemDao: ItemDao) {
+
+   suspend fun insertItem(item: Item){
+       itemDao.insertItem(item)
+    }
+
+    suspend fun getItem():
+       LiveData<List<Item>> {
+        return itemDao.getAllItems()
+    }
+
+
+}
